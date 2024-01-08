4a0b90b8ab882b3edc982baa4220fb9beb99ac5a
mskim3578 <mskim3578@naver.com>
2024-01-08 15:49:54 +0900

    test01

--- src/main/java/ch01_di/MemberDao.java
+++ src/main/java/ch01_di/MemberDao.java
@@ -6,4 +6,15 @@ import java.util.Map;
 public class MemberDao {
 	private static long nextId = 0;
 	private Map<String,Member> map = new HashMap<String,Member>();
+	
+	public Member selectByEmail(String email) {
+		return map.get(email); //Member 객체 리턴
+	}
+	public void insert(Member member) {
+		member.setId(++nextId);
+		map.put(member.getEmail(), member);
+	}
+	
+	
+	
 }
